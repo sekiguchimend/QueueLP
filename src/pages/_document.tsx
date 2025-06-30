02774f9d9e065a3ff110f94f9cc096904962849e
sekiguchimend <sekiguchishunya0619@gmail.com>
2025-06-30 14:21:27 +0900

    feat: faviconをwork_mate.pngに設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,10 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/work_mate.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/work_mate.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/work_mate.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/work_mate.png" />
       </Head>
       <body>
         <Main />
